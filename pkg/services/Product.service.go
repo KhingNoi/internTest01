7b6f934d2e47af77b65257e2347fac24fc0879ec
KhingNoi <tanapatd@odds.team>
2024-05-26 02:57:47 +0700

    add condition to send status and response according to the situation

--- pkg/services/Product.service.go
+++ pkg/services/Product.service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"internTest01/pkg/repositories"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -10,6 +11,9 @@ import (
 
 func GetProductList(context *gin.Context, db *gorm.DB) {
 	products, err := repositories.FindProductList(db)
+	if len(products) == 0 {
+		context.JSON(http.StatusNoContent, products)
+	}
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -19,10 +23,19 @@ func GetProductList(context *gin.Context, db *gorm.DB) {
 
 func GetProductById(context *gin.Context, db *gorm.DB) {
 	id := context.Param("id")
+	if _, strconvErr := strconv.Atoi(id); strconvErr != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID format"})
+		return
+	}
 	product, err := repositories.FindProductById(db, id)
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
+		if err.Error() == "record not found" {
+			context.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		} else {
+			context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 	}
 	context.JSON(http.StatusOK, product)
 }
