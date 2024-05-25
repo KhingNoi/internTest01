bfe25f3f53b13c9f2c47d2961ef571b35c35122d
KhingNoi <tanapatd@odds.team>
2024-05-26 00:00:58 +0700

    setup database connection and add db to route and main file

--- main.go
+++ main.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"internTest01/configs"
+	"internTest01/database"
 	"internTest01/routes"
 )
 
 func main() {
-	configs.InitEnvConfigs()
-	testEnv1 := configs.EnvConfigs.DatabaseName
-	testEnv2 := configs.EnvConfigs.DatabasePassword
-	testEnv3 := configs.EnvConfigs.DatabasePort
-	testEnv4 := configs.EnvConfigs.DatabaseUrl
-	testEnv5 := configs.EnvConfigs.DatabaseUser
-	fmt.Println("DatabaseName :", testEnv1)
-	fmt.Println("DatabasePassword :", testEnv2)
-	fmt.Println("DatabasePort :", testEnv3)
-	fmt.Println("DatabaseUrl :", testEnv4)
-	fmt.Println("DatabaseUser :", testEnv5)
-	routes.Router().Run(":8080")
+	db := database.ConnectDatabase()
+	defer database.CloseDB()
+	routes.Router(db).Run(":8080")
 }
--- routes/routes.go
+++ routes/routes.go
@@ -4,18 +4,20 @@ import (
 	"internTest01/pkg/controllers"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 type AppRouter struct {
 	route *gin.Engine
 }
 
-func Router() AppRouter {
+func Router(db *gorm.DB) AppRouter {
 	router := AppRouter{route: gin.Default()}
 	main := router.route.Group("/")
-	// api := router.route.Group("/api")
+	api := router.route.Group("/api")
 
 	controllers.HelpCheckController(main)
+	controllers.ProductController(api, db)
 	return router
 }
 
