bbfb309576d7b0459ac4392ecf2eb8b1cb118690
KhingNoi <tanapatd@odds.team>
2024-05-26 16:36:16 +0700

    fix bug about invalid total

--- pkg/repositories/Product.repository.go
+++ pkg/repositories/Product.repository.go
@@ -12,15 +12,17 @@ func FindProductById(db *gorm.DB, id string) (models.Product, error) {
 	return product, queryResult.Error
 }
 
-func FindProductListWithPagination(db *gorm.DB, page int, size int) ([]models.Product, int, error) {
+func FindProductListWithPagination(db *gorm.DB, page int, size int) ([]models.Product, int64, error) {
 	var productsWithPageAndSize []models.Product
-
+	var total int64
+	if err := db.Model(&models.Product{}).Count(&total).Error; err != nil {
+		return nil, 0, err
+	}
 	offset := (page - 1) * size
 	queryResult := db.Limit(size).Offset(offset).Find(&productsWithPageAndSize)
 	if queryResult.Error != nil {
 		return nil, 0, queryResult.Error
 	}
-	total := len(productsWithPageAndSize)
 	return productsWithPageAndSize, total, nil
 }
 
